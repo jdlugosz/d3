ae728d23ce7e564cd4173b31f449de08257cde6f
John M. Dlugosz <john.dlugosz@vsee.com>
2018-05-02 16:00:33 -0500

    code wrapper using "X-macro" system

--- include/d3/twostep.h
+++ include/d3/twostep.h
@@ -2,29 +2,42 @@
 
 #include <iterator>
 
+
+#if defined X
+#    error This header uses "X macros".  \
+     Either this #include is nested within another usage, or \
+     an earlier header neglected to #undef it when done.
+#endif
+
+#define X_LIST \
+	X(begin,Begin) \
+	X(end,End)
+
 namespace Dlugosz::d3 {
 
-inline namespace twostep_wrapper {
+namespace twostep_wrapper {
 // The std:: declarations need to be in scope for the two-step functions to work,
 // but we don't want them to appear as declared names in namespace twostep.  That way,
 // `using namespace twostep;` will bring in Begin but not bring in begin from std.
 
-using std::begin;
-using std::end;
 
-inline namespace twostep_inner {
+#define X(xname,XName) using std::xname;
+X_LIST
+#undef X
 
-inline auto Begin = [](auto&& r) ->
-    decltype(begin(std::forward<decltype(r)>(r))) // using return type to do SFINAE
-{
-	return begin(std::forward<decltype(r)>(r));
-};
 
-inline auto End = [](auto&& r) ->
-decltype(end(std::forward<decltype(r)>(r)))
-{
-	return end(std::forward<decltype(r)>(r));
-};
+namespace twostep_inner {
+
+#define X(xname,XName) \
+inline constexpr auto XName = [](auto&& r) \
+    noexcept(noexcept(xname(std::forward<decltype(r)>(r)))) /* ask if the code is noexcept */ \
+    -> decltype(      xname(std::forward<decltype(r)>(r)))  /* using return type to do SFINAE */ \
+        {      return xname(std::forward<decltype(r)>(r));  /* the real body to execute */ };
+
+
+X_LIST
+#undef X
+
 
 } // namespace twostep_inner
 
@@ -35,3 +48,5 @@ namespace twostep = twostep_wrapper::twostep_inner;
 using namespace twostep;
 
 }
+
+#undef X_LIST
